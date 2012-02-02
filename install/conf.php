c4031c1569736a5d5989acb5dd5da286b3a9c433
JianTing Quek <jiantingrox@gmail.com>
2012-02-02 20:35:24 +0800

    Updated to 2.5.1

--- install/conf.php
+++ install/conf.php
@@ -22,27 +22,27 @@
    */
   if (!defined('BASEPATH'))exit('Failed Hax0r');
 
-  $core['mysql']['host']     = '<host>';
-  $core['mysql']['username'] = '<username>';
-  $core['mysql']['password'] = '<password>';
-  $core['mysql']['database'] = '<database>';
+  $core['mysql']['host']        = '<host>';
+  $core['mysql']['username']    = '<username>';
+  $core['mysql']['password']    = '<password>';
+  $core['mysql']['database']    = '<database>';
 
-  $core['security']['hash']  = '<hash>';//jtHash secret hash.
-  $core['mysql']['b_sql']    = 'backup_sql.sql';//Backup SQL File. Currently in Beta.
-  $core['session']['time']   = '60 minutes';//Session timeout. For both $_SESSION and Database Storage usage.
+  $core['security']['hash']     = '<hash>';//jtHash secret hash.
+  $core['mysql']['b_sql']       = 'backup_sql.sql';//Backup SQL File. Currently in Beta.
+  $core['session']['time']      = '60 minutes';//Session timeout. For both $_SESSION and Database Storage usage.
   
-  $core['theme']['name']     = 'simplicity';//skyAdmin Theme folder name under the "_themes" folder.
+  $core['theme']['name']        = 'simplicity';//skyAdmin Theme folder name under the "_themes" folder.
 
   define('DATABASE_IP_SESSION', false);//Only enable this if you have a static IP.
-  define('SKYADMIN_BUILD', '2.5.0');//Your current skyAdmin Build.
+  define('SKYADMIN_BUILD', '2.5.1');//Your current skyAdmin Build.
   define('SKYADMIN_RETURN_ERROR', true);//You are recommended to enable this. Shows users "403" Alert or "404" not found errors.
-  define('ENABLE_SKYADMIN_CRON_JOBS', true);//Enabling cron jobs used by skyAdmin. You can disable certain jobs in the panel.
+  define('ENABLE_SKYADMIN_CRON_JOBS', false);//Enabling cron jobs used by skyAdmin. You can disable certain jobs in the panel.
   define('ENABLE_PANEL_JOBS', false);//Disable this only if you want Cron Jobs to be done when someone is successful in logging into the panel.
                                      //Enable this only if you want Cron Jobs to be done whenever the page refreshes.
   define('SKYADMIN_PREFIX', '<prefix>');//Prefix for database.
   define('jt_hash_secret', $core['security']['hash']);
   
-  session_set_cookie_params(time()+3600, '/', $_SERVER['HTTP_HOST'], isset($_SERVER["HTTPS"]), true);
+  session_set_cookie_params(time()+3600, '/', $_SERVER['HTTP_HOST'], isset($_SERVER['HTTPS']), true);
 
   require_once('jthash.enc.php');
     $hash = new jthash();
@@ -55,8 +55,13 @@
     $ui   = new skyAdmin_Ui();
   require_once('resource.inc.php');
     $res  = new skyAdmin_Resource();
-  require_once('template.class.php');
-    $temp = new skyAdmin_Template($core['theme']['name']);
+  if(!defined('ENABLE_SKYADMIN_THEME')){
+  	require_once('template.class.php');
+      $temp = new skyAdmin_Template($core['theme']['name']);
+  }elseif(ENABLE_SKYADMIN_THEME){
+  	require_once('template.class.php');
+      $temp = new skyAdmin_Template($core['theme']['name']);
+  }
   require_once('func.inc.php');
   require_once('core.func.php');
   if(ENABLE_SKYADMIN_CRON_JOBS){
